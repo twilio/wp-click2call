d5df5336ec88f5cac8bd8ee34844e4b565522458
Adam Ballai <adam@blackacid.org>
2010-01-20 00:27:20 -0800

    Some simple bounds checking before submitting to call processing

--- click2call.js
+++ click2call.js
@@ -1,14 +1,16 @@
 jQuery(document).ready(function() {
     jQuery('.click2call').each(function() {
-        swfobject.embedSWF(click2callL10n.plugin_url + '/dialer.swf', jQuery(this).attr('id'), '150px', '25px', '9.0.0');
+        swfobject.embedSWF(click2callL10n.plugin_url + '/dialer.swf', jQuery(this).attr('id'), '230px', '55px', '9.0.0');
     });
 });
 
 var StartCall = function(caller) {
-    jQuery.ajax({
-        url : click2callL10n.plugin_url + '/dialer.php',
-        data : {
-            caller : caller
-        }
-    });
+    if(caller && caller.length >= 9) {
+        jQuery.ajax({
+            url : click2callL10n.plugin_url + '/dialer.php',
+            data : {
+                caller : caller
+            }
+        });
+    }
 };
